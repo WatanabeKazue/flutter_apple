d81a34be14f31cf036d5946b2142e328d0ab1e9f
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-06-16 14:46:21 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(app);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      (appBar: AppBar,
+      title: Text("ツッコミマシーン"),
+      centerTitle: true,
+    ),
+    body: Container(),
+
+    );
+  }
+}
