1c26bc6832103f5b72139663c471e33f21452b69
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-06-16 14:59:48 +0900

    ここまでやってます

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(app);
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      (appBar: AppBar,
-      title: Text("ツッコミマシーン"),
-      centerTitle: true,
-    ),
-    body: Container(),
+    return (
 
-    );
-  }
+  };
 }
