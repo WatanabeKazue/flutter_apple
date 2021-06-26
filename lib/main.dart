de29045a0413ed8cf87a7b744134b3fce743a86c
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-06-26 11:11:24 +0900

    エラー修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_apple/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  
-  Widget widget = ElevatedButton(onPressed:()=> null, child: Text(""));
-  
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "ツッコミマシーン",
       theme: ThemeData.dark(),
       home: HomeScreen(),
-
-
-      );
-
-
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soundpool/soundpool.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +7,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // Siring _tex  // Siring _text04 = "残念でした";
+  // Siring _text05 = "不合格です";
+  // Siring _text06 = "頑張りましょう";
+
+List<String> _texts = ["おめでとうございます","合格です","よくできました","残念でした",
+  "不合格です","頑張りましょう"];
+
+List<int> _soundIds = [0,0,0,0,0,0];
+Soundpool _soundpool;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,4 +26,3 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Container(),
     );
   }
-}
