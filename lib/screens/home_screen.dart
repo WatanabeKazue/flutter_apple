2fc123ad49a9313ab767c50a4b193fe5b5119f92
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-06-27 13:33:47 +0900

    Merge remote-tracking branch 'origin/master'

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,28 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:soundpool/soundpool.dart';
 
-
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<String> _texts = [
+    "おめでとうございます",
+    "合格です",
+    "よくできました",
+    "残念でした",
+    "不合格です",
+    "頑張りましょう"
+  ];
 
-  List<String> _texts = ["おめでとうございます","合格です","よくできました","残念でした","不合格です","頑張りましょう"];
-
-
-  List<int> _soundIds = [0,0,0,0,0,0];
+  List<int> _soundIds = [0, 0, 0, 0, 0, 0];
   int _soundId = 1;
-  Soundpool _soundpool;
+  late Soundpool _soundpool;
+
+  int number = 3;
 
   @override
   void initState() {
-    _initSounds ();
-
     super.initState();
+    _initSounds();
   }
-  void _initSounds() async{
+
+  void _initSounds() async {
     _soundpool = Soundpool();
 
     _soundIds[0] = await loadSound("assets/sounds/sound1.mp3");
@@ -34,15 +40,13 @@ class _HomeScreenState extends State<HomeScreen> {
     _soundIds[4] = await loadSound("assets/sounds/sound5.mp3");
     _soundIds[5] = await loadSound("assets/sounds/sound6.mp3");
 
-   // _soundIds[0] = await rootBundle
-       // .load("assets/sounds/sound1.mp3")
-       // .then((value) => _soundpool.load(value));
-
+    // _soundIds[0] = await rootBundle
+    // .load("assets/sounds/sound1.mp3")
+    // .then((value) => _soundpool.load(value));
   }
 
-  Future<int> loadSound(String soundpath) {
-     return rootBundle.load(soundpath).then((value) => _soundpool.load(value));
-
+  Future<int> loadSound(String soundPath) {
+    return rootBundle.load(soundPath).then((value) => _soundpool.load(value));
   }
 
   @override
@@ -54,67 +58,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text("ツッコミマシーン"),
-    centerTitle: true,
-    ),
-    body: Container(
-    padding: EdgeInsets.all(8.0),
-    child:Column(
-    children: <Widget>[
-
-    Expanded(flex : 1,
-    child:Row(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [
-    Expanded(flex: 1,child: _soundButton(_texts[0],_soundIds[0])),//TODO  ボタン
-    Expanded(flex: 1,child: _soundButton(_texts[1],_soundIds[1])),//TODO  ボタン
-    ],
-    ),
-    ),
-    Expanded(
-    flex: 1,
-    child: Row(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [
-    Expanded(flex: 1,child: _soundButton(_texts[2],_soundIds[2])),//TODO  ボタン
-    Expanded(flex: 1,child: _soundButton(_texts[3],_soundIds[3])),//TODO  ボタン
-    ],
-    ),
-    ),
-    Expanded(
-    flex:1,
-    child: Row(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children:[
-    Expanded(flex: 1,child: _soundButton(_texts[4],_soundIds[4])),//TODO  ボタン
-    Expanded(flex: 1,child: _soundButton(_texts[5],_soundIds[5])),//TODO  ボタン
-    ],
-
-
-    ),
-    )
-    ,
-    ]
-    ,
-    ),
-    );
-  }
-
-  Widget _soundButton() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        onPressed: () => null, //TODO 後で書き換える
-        child: Text(""), //TODO 後で書き換える
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(flex: 1,child: _soundButton(_texts[0],_soundIds[0])),//TODO  ボタン
+                  Expanded(flex: 1,child: _soundButton(_texts[1],_soundIds[1])),//TODO  ボタン
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(flex: 1,child: _soundButton(_texts[2],_soundIds[2])),//TODO  ボタン
+                  Expanded(flex: 1,child: _soundButton(_texts[3],_soundIds[3])),//TODO  ボタン
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(flex: 1,child: _soundButton(_texts[4],_soundIds[4])),//TODO  ボタン
+                  Expanded(flex: 1,child: _soundButton(_texts[5],_soundIds[5])),//TODO  ボタン
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-
-
-
+  Widget _soundButton(String displayText,int soundId) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          onPressed: () => null, //TODO 後で書き換える
+          child: Text(displayText), //TODO 後で書き換える
+        ),
+      ),
+    );
+  }
 }
-  
-
-
